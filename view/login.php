5c0392dd35e18e2565cb9d4ac08d1ac74e8a47ad
yupa-sdm <86347328+yupa-sdm@users.noreply.github.com>
2022-11-22 11:43:48 +0700

    Update login.php

--- view/login.php
+++ view/login.php
@@ -1,7 +1,14 @@
 <section class="vh-100">
 <div class="container">
   <div class="row">
-    <div class="col-sm-8">col-sm-4</div>
+    <div class="col-sm-8">
+      <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
     <div class="col-sm-4">col-sm-8</div>
   </div>
 </div>
