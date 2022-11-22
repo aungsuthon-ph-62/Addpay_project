d0ef0a47eb23722aab9e553d159eb5c8a9e33792
● YOUNGBOY MONEZXD ● <85674453+aungsuthon-ph-62@users.noreply.github.com>
2022-11-22 15:43:49 +0700

    All update

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
 <?php
 include 'layout/head.php';
 ?>
+<style>
+    body {
+        font-family: "Kanit", sans-serif;
+        font-family: "Noto Sans", sans-serif;
+        font-family: "Noto Sans Thai", sans-serif;
+        font-family: "Poppins", sans-serif;
+        font-family: "Prompt", sans-serif;
+    }
+</style>
 
 <body>
     <?php if (isset($_GET['login'])) {
-        include 'view/login.php';
-        exit;
+        require 'view/login.php';
     } else if (isset($_GET['register'])) {
-        include 'view/login.php';
-        exit;
+        require 'view/login.php';
     } else {
-        include 'view/dashboard.php';
-        exit;
+        require 'view/dashboard.php';
     }
     ?>
 
-    <?php
-    include 'layout/footer.php';
-    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ layout/dashboardNav.php
@@ -0,0 +1,8 @@
+<header class="navbar navbar-dark sticky-top dashboardNav-bg flex-md-nowrap p-3 p-md-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">
+        <img src="image/logo-removebg-preview (1).png" class="img-fluid" alt="" style="width:100px">
+    </a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed mt-3" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</header>
