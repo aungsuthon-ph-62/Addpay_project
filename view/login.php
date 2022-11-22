582adeefc7a1bba25921e99ac556a0e6f9f8d8c2
Thanawat-github <85674229+Thanawat-github@users.noreply.github.com>
2022-11-22 15:48:20 +0700

    profilesuccess

--- view/login.php
+++ view/login.php
@@ -2,8 +2,7 @@
   <div class="container py-5 h-100">
     <div class="row d-flex align-items-center justify-content-center h-100">
       <div class="col-md-8 col-lg-7 col-xl-6">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-          class="img-fluid" alt="Phone image">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image">
       </div>
       <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
         <form>
@@ -35,12 +34,10 @@
             <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
           </div>
 
-          <a class="btn btn-primary btn-lg btn-block" style="background-color: #3b5998" href="#!"
-            role="button">
+          <a class="btn btn-primary btn-lg btn-block" style="background-color: #3b5998" href="#!" role="button">
             <i class="fab fa-facebook-f me-2"></i>Continue with Facebook
           </a>
-          <a class="btn btn-primary btn-lg btn-block" style="background-color: #55acee" href="#!"
-            role="button">
+          <a class="btn btn-primary btn-lg btn-block" style="background-color: #55acee" href="#!" role="button">
             <i class="fab fa-twitter me-2"></i>Continue with Twitter</a>
 
         </form>
