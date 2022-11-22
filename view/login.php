95029f276d10468378ec1b50b11ebb147d229817
yupa-sdm <86347328+yupa-sdm@users.noreply.github.com>
2022-11-22 11:53:57 +0700

    Update login.php

--- view/login.php
+++ view/login.php
@@ -28,20 +28,6 @@
             <a href="#!">Forgot password?</a>
           </div>
 
-          <div class="container">
-            <div class="row">
-              <div class="col-sm-8">
-                <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-4">col-sm-8</div>
-            </div>
-          </div>
-
           <!-- Submit button -->
           <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
 
