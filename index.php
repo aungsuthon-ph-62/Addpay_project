d31868d8a53a98bfa4e39704d1ee69570651482c
Thanawat-github <85674229+Thanawat-github@users.noreply.github.com>
2022-11-22 15:53:58 +0700

    tryagain

--- index.php
+++ index.php
@@ -5,12 +5,13 @@ include 'layout/head.php';
 <body>
     <?php if (isset($_GET['login'])) {
         include 'view/login.php';
+        exit;
     } else if (isset($_GET['register'])) {
         include 'view/login.php';
-    } else if (isset($_GET['profile'])) {
-        include 'view/dashboard/profile.php';
+        exit;
     } else {
         include 'view/dashboard.php';
+        exit;
     }
     ?>
 
