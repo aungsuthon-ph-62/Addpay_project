ed98ef62c6c16d4bbf8218b9b2085b699779303b
● YOUNGBOY MONEZXD ● <85674453+aungsuthon-ph-62@users.noreply.github.com>
2022-11-24 10:04:17 +0700

    move bootstrap script

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ include 'layout/head.php';
     }
     ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    
 
 </body>
 
--- layout/head.php
+++ layout/head.php
@@ -33,6 +33,7 @@
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Bootstrap -->
 
     <!-- Font Awesome -->
