f4e48bab9f156ecd466ca536d54ff381a2a621ad
● YOUNGBOY MONEZXD ● <85674453+aungsuthon-ph-62@users.noreply.github.com>
2022-11-25 11:12:13 +0700

    update dashboard nav

--- layout/dashboardNav.php
+++ layout/dashboardNav.php
@@ -4,16 +4,16 @@
             <a class="navbar-brand ps-3 ps-md-5 m-0" href="#">
                 <img src="image/logo-removebg-preview (1).png" class="img-fluid" alt="Addpay logo" style="width: auto; height: 2rem;">
             </a>
-            <h6 class="d-none d-md-block text-white">ระบบบริหารจัดการงานบริหาร</h6>
+            <h5 class="d-none d-md-block text-white">ระบบบริหารจัดการงานบริหาร</h5>
             <button class="navbar-toggler position-absolute d-md-none collapsed mt-3" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
         </div>
-        <div class="col-12 col-md-3 d-flex justify-content-around align-items-center">
-            <a href="#" class="text-decoration-none">
+        <div class="col-12 col-md-3 d-flex justify-content-end align-items-center">
+            <!-- <a href="#" class="text-decoration-none">
                 <img src="image/addpaylogo.png" alt="Profile Picture" class="img-fluid rounded-circle shadow " style="width: auto; height: 3rem;">
-            </a>
-            <a href="#" class="btn btn-danger">ออกจากระบบ <i class="fa-solid fa-right-from-bracket"></i></a>
+            </a> -->
+            <a href="#" class="btn btn-danger ">ออกจากระบบ <i class="fa-solid fa-right-from-bracket"></i></a>
         </div>
     </div>
 </header>
--- view/dashboard.php
+++ view/dashboard.php
@@ -15,7 +15,7 @@ include 'layout/dashboardNav.php';
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <h1 class="h5 pt-5">หน้าหลัก</h1>
             <hr>
-
+            
 
         </main>
     </div>
