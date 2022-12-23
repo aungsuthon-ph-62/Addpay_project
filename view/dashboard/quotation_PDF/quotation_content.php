a5371bb0c1fa7b7c4d3261c0ee70635ebc4329ba
lima01sh <85719221+lima01sh@users.noreply.github.com>
2022-12-23 16:57:13 +0700

    update quotation

---
+++ view/dashboard/quotation_PDF/quotation_content.php
@@ -0,0 +1,33 @@
+<div id="quotationForm" class="container pb-md-0 mb-5 mt-5">
+    <table>
+        <tr>
+            <td style="width:300px">
+                <div class="logo">
+                    <img src="../../image/addpay-form-text.png" class="img-fluid position-relative" width="200" hight="auto" alt="addpay_logo_form">
+                </div>
+            </td>
+            <td style="width:542px">
+                <div style="margin-left: 40px;">
+                    <b class="text-left"> บริษัท แอดเพย์ เซอร์วิสพอยท์ จํากัด</b><br>
+                    <p class="text-left">406 หมู่ 18 ตําบลขามใหญ่ อําเภอเมือง จังหวัดอุบลราชธานี โทร. 045-317123</p>
+                </div>
+            </td>
+        </tr>
+    </table>
+    <table>
+
+        <tr>
+            <td style="width:200px;  border: 1px solid;">
+                <b class="text-center">ข้อมูลใบเสนอราคา</b><br>
+                <b class="text-center">Quotation</b>
+            </td>
+            <td style="width:442px"></td>
+            <td style="width:200px">
+                <p class="text-right">เลขที่ / No. <u> <sup>6401001<sup> </u> </p><br>
+                <p class="text-right">วันที่ / Date. <u> <sup>11/1/2564<sup> </u> </p>
+            </td>
+        </tr>
+
+    </table>
+
+</div>
--- view/dashboard/quotation_appraisal_form.php
+++ view/dashboard/quotation_appraisal_form.php
@@ -1,118 +1,72 @@
 <?php
+include("./quotation_PDF/quotation_head.php");
 
 require_once __DIR__ . '../../../vendor/autoload.php';
 
+$defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
+$fontDirs = $defaultConfig['fontDir'];
 
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->allow_charset_conversion = true;
-$mpdf->charset_in = 'macthai';
 
-ob_start();
+$defaultFontConfig = (new Mpdf\Config\FontVariables())->getDefaults();
+$fontData = $defaultFontConfig['fontdata'];
+
+$mpdf = new \Mpdf\Mpdf([
+    'fontDir' => array_merge($fontDirs, [
+        __DIR__ . '/fonts',
+    ]),
+    'fontdata' => $fontData + [
+            'sarabun' => [
+                'R' => 'THSarabunNew.ttf',
+                'I' => 'THSarabunNew Italic.ttf',
+                'B' =>  'THSarabunNew Bold.ttf',
+            ]
+        ],
+        'default_font' => 'sarabun',
+        'default_font_size' => 18,
+]);
+
+// $mpdf = new \Mpdf\Mpdf([
+//     'mode' => 'utf-8', 'format' => 'A4-P',
+//     'fontdata' => $fontData + [
+//         'Sarabun' => [
+//             'R' => 'Sarabun-Regular.ttf',
+//             'I' => 'Sarabun-Italic.ttf',
+//         ]
+//     ]
+// ]);
+
+$a = file_get_contents("./quotation_PDF/quotation_content.php");
+
+$mpdf->WriteHTML($a);
+$mpdf->Output('./quotation_PDF/quotation_appraisal0.pdf'); //link web of file pdf
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        Addpay-Project
-    </title>
-  
-    <!-- FAVICON -->
-    <link rel="shortcut icon" href="image/addpaylogo.png" type="image/x-icon">
-    <!-- FAVICON -->
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Bootstrap -->
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <!-- Font Awesome -->
-
-    <!-- Font Form -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet">
-    <!-- Font Form -->
-
-</head>
 <style>
     body {
         font-family: 'Sarabun', sans-serif;
+    
+    }
+    .text-center{
+        justify-items: center;
+        align-items: center;
+    }
+    .text-left{
+        justify-items: left;
+    }
+    .text-right{
+        justify-items: right;
     }
 </style>
 
 <body>
-    <div class="container  py-md-5 px-md-4" style="width: 80%;">
-        <div class="main-body">
-            <div id="quotationForm" class="container pb-md-0 mb-5 mt-5">
-                <div class="row text-center">
-                    <div class="col-md-4 p-md-2">
-                        <div class="text-center">
-                            <img src="../../image/addpay-form-text.png" class="img-fluid position-relative" style="width: 18rem;" alt="addpay_logo_form">
-                        </div>
-                        <div class="text-center p-md-3 px-md-5">
-                            <div class="border border-dark border-3 p-md-3 px-md-4">
-                                <p class="fw-bold mb-md-4">ข้อมูลใบเสนอราคา</p>
-                                <p class="fw-bold">Quotation</p>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="col-md-8 p-md-2 pt-md-5">
-                        <div class="text-center mb-md-5">
-                            <p class="fw-bold mb-md-4">บริษัท แอดเพย์ เซอร์วิสพอยท์ จํากัด</p>
-                            <p class="">406 หมู่ 18 ตําบลขามใหญ่ อําเภอเมือง จังหวัดอุบลราชธานี โทร. 045-317123</p>
-                        </div>
-                        <div class="float-end col-md-5">
-                            <div class="text-center">
-                                <div class="row g-3 align-items-center mb-3">
-                                    <div class="col-md-4">
-                                        <label for="noForm" class="col-form-label">เลขที่/No.</label>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <input type="text" id="noForm" class="form-control border border-2 border-dark border-start-0 border-top-0 border-end-0 rounded-0" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="text-center  ">
-                                <div class="row g-3 align-items-center mb-3">
-                                    <div class="col-md-4">
-                                        <label for="DateForm" class="col-form-label">วันที่/Date.</label>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <input type="text" id="DateForm" class="form-control border border-2 border-dark border-start-0 border-top-0 border-end-0 rounded-0" required>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        
+    <div class="container  py-md-5 px-md-4" style="width: 100%;">
         <?php
-
-        ob_end_flush();
-
-        $html = ob_get_contents();
-        $mpdf->WriteHTML($html);
-        $mpdf->Output('quotation_appraisal.pdf');
-        
+        include("./quotation_PDF/quotation_content.php");
         ?>
-
-        
     </div>
     <div class="mx-auto d-flex justify-content-end me-5">
-        <a class="btn btn-danger px-2 px-md-4 mt-2 rounded-3 fs-5 fw-bold " role="button" href="./quotation_appraisal.pdf"><i class="fa-solid fa-print"></i> พิมพ์เอกสาร</a>
+        <a class="btn btn-danger px-2 px-md-4 mt-2 rounded-3 fs-5 fw-bold " role="button" href="./quotation_PDF/quotation_appraisal0.pdf"><i class="fa-solid fa-print"></i> พิมพ์เอกสาร</a>
     </div>
 </body>
-
-
-</html>
