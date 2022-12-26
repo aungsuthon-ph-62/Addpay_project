5a6697a983437560825889f0f8efc3dad416a7e0
yuphawadee <93894993+yuphawadee@users.noreply.github.com>
2022-12-26 15:43:59 +0700

    docout_pdf

--- view/dashboard/docout_PDF/docout_content.php
+++ view/dashboard/docout_PDF/docout_content.php
@@ -1,135 +1,18 @@
-<!-- <style>
-    .right {
-        padding-left: px;
-        padding-right: 100px;
-    }
-
-    td {
-        padding-top: 10px;
-    }
-</style>
-<div id="quotationForm" class="container pb-md-0 mb-5 mt-5">
-    <div>
-    <table>
-            <tr>
-                <th>
-                    <img src="../../../image/logo-addpay.png" alt="" width="80%">
-                </th>
-                <th class="right">
-                    <label for="">บริษัท แอดเพย์ เซอร์วิสพอยท์ จำกัด
-                        ADDPAY SERVICE POINT CO.,LTD.
-                        406 หมู่ 18 ตำบลขามใหญ่ อำเภอเมือง จังหวัดอุบลราชธานี</label>
-                </th>
-            </tr>
-        </table>
-        <table>
-            <tr>
-                <td style="padding-top: 20px;">ที่ อพ.</td>
-                <td style="padding-top: 20px;">020/2563</td>
-            </tr>
-        </table>
-        <table style="width: 100%;">
-            <tr>
-                <td style="text-align: center;">9 มิถุนายน 2563</td>
-            </tr>
-        </table>
-        <table>
-            <tr>
-                <td style="padding-top: 20px;">เรื่อง</td>
-                <td style="padding-top: 20px; padding-left:10px">ขอเชิญบุคลากรในสังกัดเป็นวิทยากรการ</td>
-            </tr>
-        </table>
-        <table>
-            <tr>
-                <td>เรียน</td>
-                <td style=" padding-left:10px">คณบดี คณะเทคโนโลยีอุตสาหกรรม มหาวิทยาลัยราชภัฎอุบลราชธานี</td>
-            </tr>
-        </table>
-        <table>
-            <tr>
-                <td>สิ่งที่ส่งมาด้วย</td>
-                <td style=" padding-left:10px">1. กำหนดการ 1 ฉบับ</td>
-            </tr>
-        </table>
-        
-        
-        <table>
-            <tr>
-                <td>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ตามที่บริษัท แอดเพย์ เซอร์วิสพอยท์ จำกัด ร่วมกับสำนักงานพัฒนาชุมชนจังหวัดสุโขทัย กำหนดจัดโครงการสัมมนาเชิงปฎิบัติการพัฒนาศักยภาพผู้ผลิต ผู้ประกอบการ OTOP และกิจกรรมพัฒนายกระดับคุณภาพมาตรฐานผลิตภัณฑ์ OTOP นั้น
-                </td>
-            </tr>
-        </table>
-        
-        <table>
-            <tr>
-                <td>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ในการนี้บริษัท แอดเพย์ เซอร์วิสพอยท์ จำกัดและสำนักงานพัฒนาชุมชนจังหวัดสุโขทัย เห็นว่าบุคลากรในสังกัดของท่าน ได้แก่ นางนริศรา สารีบุตร ตำแหน่งอาจารย์ประจำสาขาวิชาการออกแบบผลิตภัณฑ์ มหาวิทยาลัยราชภัฎอุบลราชธานี เป็นผู้มีความรู้และความสามารถ เป็นวิทยากรในโครงการดังกล่าว โดยมีนางสาวสมศรี วรรณู เป็นผู้ติดต่อประสานงาน
-                </td>
-            </tr>
-        </table>
-        <table>
-            <tr>
-                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;จึงเรียนมาเพื่อโปรดทราบและให้ความอนุเคราะห์ด้วย จักขอบคุณยิ่ง</td>
-            </tr>
-        </table>
-        <table style="width: 100%;">
-            <tr>
-                <td style="text-align: center;padding-top:50px">
-                    ขอแสดงความนับถือ <br><br><br><br>
-                    ( ดร.ศิริมาเมธ์วดี ศิรธนิตรา )<br>
-                    ประธานกรรมการ <br>
-                    บริษัท แอดเพย์ เซอร์วิสพอยท์ จำกัด
-                </td>
-            </tr>
-        </table>
-        <div style="padding-top: 100px;">
-            <label for="">ผู้ประสานงาน<br>
-                โทร. 085-4964855 , 045-317123<br>
-                แฟกซ์ 045-317678
-            </label>
-        </div>
-    </div>
-
-
-</div>-->
-
-
-
-
- 
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Sarabun:wght@200;300;400;500;600;700&display=swap');
-
-    body {
-        font-family: 'Sarabun', sans-serif;
-    }
-
-    .right {
-        padding-left: 80px;
-        padding-right: 100px;
-    }
-
-    td {
-        padding-top: 10px;
-    }
-</style>
-
-<div class="container py-5">
+<div class="container py-5" style="width: 842px;">
     <div class="main-body">
         <table>
             <tr>
                 <td>
                 <th>
-                    <img src="../../../image/logo-addpay.png" alt="" width="80%">
-                </td>
+                    <img src="../../image/logo-addpay.png" alt="" width="200" hight="auto">
+                    </td>
                 <td class="right">
-                </th>
+                    </th>
                 <th class="right">
                     <label for="">บริษัท แอดเพย์ เซอร์วิสพอยท์ จำกัด
                         ADDPAY SERVICE POINT CO.,LTD.
                         406 หมู่ 18 ตำบลขามใหญ่ อำเภอเมือง จังหวัดอุบลราชธานี</label>
-                </td>
+                    </td>
                 </th>
             </tr>
         </table>
@@ -162,8 +45,8 @@
                 <td style=" padding-left:10px">1. กำหนดการ 1 ฉบับ</td>
             </tr>
         </table>
-        
-        
+
+
         <table>
             <tr>
                 <td>
@@ -171,7 +54,7 @@
                 </td>
             </tr>
         </table>
-        
+
         <table>
             <tr>
                 <td>
@@ -194,7 +77,7 @@
                 </td>
             </tr>
         </table>
-        <div style="padding-top: 100px;">
+        <div style="padding-top: 50px;">
             <label for="">ผู้ประสานงาน<br>
                 โทร. 085-4964855 , 045-317123<br>
                 แฟกซ์ 045-317678
@@ -203,45 +86,3 @@
 
     </div>
 </div>
-
-
-
-<!-- <div id="quotationForm" class="container pb-md-0 mb-5 mt-5">
-    <div>
-        <table>
-            <tr>
-                <td style="width:250px">
-                    <div class="logo">
-                        <img src="../../../image/addpay-form-text.png" class="img-fluid position-relative" width="200" hight="auto" alt="addpay_logo_form">
-                    </div>
-                </td>
-                <td style="width:592px">
-                    <div style="margin-left: 40px;">
-                        <b class="text-left"> บริษัท แอดเพย์ เซอร์วิสพอยท์ จํากัด</b><br>
-                        <p class="text-left">406 หมู่ 18 ตําบลขามใหญ่ อําเภอเมือง จังหวัดอุบลราชธานี โทร. 045-317123</p>
-                    </div>
-                </td>
-            </tr>
-        </table>
-    </div>
-    
-    <div>
-        <table>
-            <tr>
-                <td style="width:150px">
-                    <p class="text-right gapline">ที่ อพ.</p>
-                    <p class="text-right gapline">ลูกค้า /หน่วยงาน </p>
-                    <p class="text-right gapline">ที่อยู่ </p>
-                </td>
-                <td style="width:692px">
-                    <p class="text-right underline gapline"> <span>ตู้พ่นน้ำ ยาฆ่าเชื้อโควิคเคลีย &nbsp;&nbsp;</span> </p>
-                    <p class="text-right underline gapline"> <span>&nbsp; บริษัท อาเคโบโน เบรค(ประเทศไทย) จำกัด &nbsp;&nbsp;</span> </p>
-                    <p class="text-right underline gapline"> <span>&nbsp;700/800 นิคมอุตสาหกรรมอมตะซิตี้ ชลบุรี หมู่ที่ 1 ตำบลพานทอง อำเภอพานทอง จังหวัดชลบุรี 20160 &nbsp;&nbsp;</span> </p>
-                </td>
-            </tr>
-        </table>
-    </div>
-   
-
-
-</div> -->
--- view/dashboard/docout_form.php
+++ view/dashboard/docout_form.php
@@ -22,20 +22,21 @@ $mpdf = new \Mpdf\Mpdf([
         ]
     ],
     'default_font' => 'sarabun',
-    'default_font_size' => 18,
+    'default_font_size' => 16,
 ]);
 
 
-$a = file_get_contents('./quotation_PDF/quotation_content.php');
+$a = file_get_contents('./docout_PDF/docout_content.php');
 $stylesheet = file_get_contents('./quotation_PDF/quotation_PDF.css');
 $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
-$mpdf->WriteHTML($a,\Mpdf\HTMLParserMode::HTML_BODY);
-$mpdf->Output('./quotation_PDF/quotation_appraisal0.pdf'); //link web of file pdf
+$mpdf->WriteHTML($a, \Mpdf\HTMLParserMode::HTML_BODY);
+$mpdf->Output('./docout_PDF/docout_PDF.pdf'); //link web of file pdf
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -53,16 +54,30 @@ $mpdf->Output('./quotation_PDF/quotation_appraisal0.pdf'); //link web of file pd
     <!-- CSS -->
 
 </head>
+<style>
+    body {}
 
+    .right {
+        padding-left: 80px;
+        padding-right: 100px;
+    }
+
+    td {
+        padding-top: 10px;
+    }
+</style>
 
 <body>
+
     <div class="container  py-md-5 px-md-4" style="width: 100%;">
+        <div class="mx-auto d-flex justify-content-end me-5">
+            <a class="btn btn-danger px-2 px-md-4 mt-2 rounded-3 fs-5 fw-bold " role="button" href="./docout_PDF/docout_PDF.pdf"><i class="fa-solid fa-print"></i> พิมพ์เอกสาร</a>
+        </div>
         <?php
         include("./docout_PDF/docout_content.php");
         ?>
     </div>
-    <div class="mx-auto d-flex justify-content-end me-5">
-        <a class="btn btn-danger px-2 px-md-4 mt-2 rounded-3 fs-5 fw-bold " role="button" href="./quotation_PDF/quotation_appraisal0.pdf"><i class="fa-solid fa-print"></i> พิมพ์เอกสาร</a>
-    </div>
+
 </body>
+
 </html>
